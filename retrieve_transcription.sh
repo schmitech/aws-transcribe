0084fb1ded3745d64b3f2b10c28a43a5534e1e6a
schmitech <remsy.schmilinsky@schmitech.ca>
2025-01-22 12:00:56 -0500

    Further enhancements
    Added minor updates

--- retrieve_transcription.sh
+++ retrieve_transcription.sh
@@ -5,6 +5,32 @@ if [ -f .env ]; then
     export $(cat .env | xargs)
 fi
 
+# Function to check the status of the transcription job
+check_status() {
+    local status
+    echo "Waiting for transcription job to complete..."
+    while true; do
+        status=$(aws transcribe get-transcription-job \
+            --profile $AWS_PROFILE \
+            --transcription-job-name $TRANSCRIPTION_JOB_NAME \
+            --query 'TranscriptionJob.TranscriptionJobStatus' \
+            --output text)
+
+        if [ "$status" == "COMPLETED" ]; then
+            echo "Transcription job completed."
+            break
+        elif [ "$status" == "FAILED" ]; then
+            echo "Transcription job failed."
+            exit 1
+        fi
+
+        sleep 30  # Wait for 30 seconds before checking again
+    done
+}
+
+# Check the status of the transcription job
+check_status
+
 # Retrieve Transcription Result
 aws s3 cp s3://$OUTPUT_BUCKET/$TRANSCRIPTION_JOB_NAME.json . \
     --profile $AWS_PROFILE
@@ -18,4 +44,4 @@ python extract_conversation.py
 aws s3 rm s3://$OUTPUT_BUCKET/$TRANSCRIPTION_JOB_NAME.json \
     --profile $AWS_PROFILE
 
-echo "Transcription result deleted from S3 bucket: $TRANSCRIPTION_JOB_NAME.json"
+echo "Transcription result deleted from S3 bucket: $OUTPUT_BUCKET"
--- start_transcription.sh
+++ start_transcription.sh
@@ -19,6 +19,23 @@ S3_BUCKET=$2
 MEDIA_FILE=$(basename "$AUDIO_FILE_PATH")
 MEDIA_FORMAT="${MEDIA_FILE##*.}"
 
+# Check if a transcription job with the same name already exists
+existing_job_status=$(aws transcribe get-transcription-job \
+    --profile $AWS_PROFILE \
+    --transcription-job-name $TRANSCRIPTION_JOB_NAME \
+    --query 'TranscriptionJob.TranscriptionJobStatus' \
+    --output text 2>/dev/null)
+
+if [ "$existing_job_status" == "IN_PROGRESS" ] || [ "$existing_job_status" == "QUEUED" ]; then
+    echo "A transcription job with the name $TRANSCRIPTION_JOB_NAME is already in progress."
+    exit 1
+elif [ "$existing_job_status" == "COMPLETED" ] || [ "$existing_job_status" == "FAILED" ]; then
+    echo "Deleting existing transcription job: $TRANSCRIPTION_JOB_NAME"
+    aws transcribe delete-transcription-job \
+        --profile $AWS_PROFILE \
+        --transcription-job-name $TRANSCRIPTION_JOB_NAME
+fi
+
 # Upload the audio file to the specified S3 bucket, overwriting if it exists
 aws s3 cp "$AUDIO_FILE_PATH" s3://$S3_BUCKET/$MEDIA_FILE --profile $AWS_PROFILE --acl bucket-owner-full-control
 
